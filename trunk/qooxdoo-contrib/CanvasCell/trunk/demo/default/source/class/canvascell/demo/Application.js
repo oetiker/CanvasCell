04f1d8a7b8c9ea709ed71bc29ec625cca422bdb5
oetiker <oetiker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-08-26 13:11:23 +0000

    initial drop of the Canvas Cell Contribution

---
+++ trunk/qooxdoo-contrib/CanvasCell/trunk/demo/default/source/class/canvascell/demo/Application.js
@@ -0,0 +1,96 @@
+/* ************************************************************************
+
+   Copyright: Public Domain
+   Authors: Tobi Oetiker <tobi@oetiker.ch>
+
+************************************************************************ */
+
+/**
+ * This is the main application class of your custom application "canvascell"
+ */
+qx.Class.define("canvascell.demo.Application", {
+    extend : qx.application.Standalone,
+    members : {
+        main : function() {
+            this.base(arguments);
+            if (qx.core.Variant.isSet("qx.debug", "on")) {
+                qx.log.appender.Native;
+                qx.log.appender.Console;
+            }
+
+            var win = new qx.ui.window.Window("CanvasCellRenderer").set({
+                layout : new qx.ui.layout.Grow(),
+                allowClose: false,
+                allowMinimize: false,
+                contentPadding: 0,
+                width: 500,
+                height: 300
+            });
+            win.addListener('appear',function(){
+                win.center();
+            });
+            win.open();
+
+            // table model
+            var tableModel = new qx.ui.table.model.Simple();
+            tableModel.setColumns([ this.tr("ID"), "Bar", "Spark", "TwoBar" ]);
+            tableModel.setData(this.createRandomRows(100));
+            // table
+            var table = new qx.ui.table.Table(tableModel).set({
+                decorator: null
+            })
+
+            var tColMod = table.getTableColumnModel();
+    
+            var barRenderer = new canvascell.Renderer(
+                new canvascell.plotter.Bar({
+                    fill   : '#280',
+                    border : '#260'
+                })
+            );
+            tColMod.setDataCellRenderer(1,barRenderer);
+
+            var sparkRenderer = new canvascell.Renderer(
+                new canvascell.plotter.SparkLine({
+                    lineWidth   : 0.5,
+                    lineColor   : '#228',
+                    sparkRadius : 1,
+                    sparkColor  : '#f22',
+                    singleScale : true,
+                    depth       : 30
+                })
+            );
+            tColMod.setDataCellRenderer(2,sparkRenderer);
+
+            var twoBarRenderer = new canvascell.Renderer(
+                new canvascell.plotter.TwoBar({
+                    mainbarFill    : '#b00',
+                    mainbarBorder  : '#a00',
+                    stackbarFill   : '#b80',
+                    stackbarBorder : '#a70'
+                })
+            );
+            tColMod.setDataCellRenderer(3,twoBarRenderer);
+
+            win.add(table);
+        },
+        createRandomRows: function(rowCount) {
+            var rowData = [];
+            var now = new Date().getTime();
+            var dateRange = 400 * 24 * 60 * 60 * 1000; // 400 days
+            var nextId = 0;
+            for (var row = 0; row < rowCount; row++) {
+                var spark = [];
+                for (var i=0;i<30;i++){
+                    spark.push(Math.random()*100);
+                }
+                var two = {
+                    mainbar: Math.random()*10,
+                    stackbar: Math.random()*20
+                };
+                rowData.push([row,row,spark,two]);
+            }
+            return rowData;
+        }
+    }
+});
---
+++ trunk/qooxdoo-contrib/CanvasCell/trunk/readme.txt
@@ -0,0 +1,9 @@
+CanvasCell
+==========
+
+A canves cellrender for the qooxdoo table widget. It has the ability to
+create tiny plots and embed them into a table cell. The plotting is done by
+plotter objects which allows for easy extensablity.
+
+Tobi Oetiker 
+http://tobi.oetiker.ch
